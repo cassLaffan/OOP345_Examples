eae1f2a9c68ce5f6ecb61883e3f15be92cf40163
cassLaffan <poisonberryvines@gmail.com>
2025-01-09 12:20:55 -0500

    Adding some of week 2's examples

---
+++ Week_2/Block_Scope.cpp
@@ -0,0 +1,43 @@
+/*
+* This is a modified example from the notes. It demonstrates
+* some new features of C++17 and some scope features. Notice
+* How we can redefine j?
+*
+* Note: for those getting warnings about versioning from this code,
+* add -std=c++17 after g++ when compiling.
+*/
+
+#include <iostream>
+
+int main()
+{
+    int i;
+    std::cout << "Enter i: ";
+    std::cin >> i;
+
+	// Introduces the variable j within the condition itself
+	// which is a feature new to C++17! 
+    if (int j = i % 10; j < 5){ // a feature of C++17
+        i -= j;      // round down
+    }
+    else{
+        i += 10 - j; // round up
+    }
+    std::cout << i << std::endl;
+
+    std::cout << "Enter i: ";
+    std::cin >> i;
+    switch (int j = i % 10; j / 5){ // a feature of C++17
+    	case 0: // round down
+			i -= j;
+			break;
+		case 1: // round up
+			i += 10 - j;
+			break;
+		default:
+			std::cout << "Default" << std::endl;
+    }
+    std::cout << i << std::endl;
+	// Try uncommenting the following line!
+	// std::cout << j << std::endl;
+}
