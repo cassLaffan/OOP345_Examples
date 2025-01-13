ec79572726b40820c57b57bbd36876f250abe02d
cassLaffan <poisonberryvines@gmail.com>
2025-01-13 14:08:40 -0500

    Fixing a stupid error

--- Week_1/Shape_Main.cpp
+++ Week_1/Shape_Main.cpp
@@ -4,19 +4,19 @@
 int main(){
 	// Now, we can have a concrete version of this class, not taking advantage of polymorphism...
 	Circle circleOne(4.2);
-	std::cout << "The area of the circle is: %.2f\n" << circleOne.area() << std::endl;
-	std::cout << "The parameter of the circle is: %.2f\n" << circleOne.parameter() << std::endl;
+	std::cout << "The area of the circle is: " << circleOne.area() << std::endl;
+	std::cout << "The parameter of the circle is: " << circleOne.parameter() << std::endl;
 
 	// Or we can have a chameleon object!
 	Shape* aShape;
 	aShape = new Square(2.2, 2.5);
-	std::cout << "The area of our generic shape is: %.2f\n" << aShape->area() << std::endl;
-	std::cout << "The parameter of the circle is: %.2f\n" << aShape->parameter() << std::endl;
+	std::cout << "The area of our generic shape is: " << aShape->area() << std::endl;
+	std::cout << "The parameter of the circle is: " << aShape->parameter() << std::endl;
 
 	// Freeing the memory
 	delete aShape;
 
 	aShape = new Circle(3.4);
-	std::cout << "The area of our generic shape is: %.2f\n" << aShape->area() << std::endl;
-	std::cout << "The parameter of the circle is: %.2f\n" << aShape->parameter() <<std::endl;
+	std::cout << "The area of our generic shape is: " << aShape->area() << std::endl;
+	std::cout << "The parameter of the circle is: " << aShape->parameter() <<std::endl;
 }
