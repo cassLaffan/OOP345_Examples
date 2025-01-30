ecad0c5ca153f3ea7adde83cd63afe8706eec9d1
cassLaffan <poisonberryvines@gmail.com>
2025-01-30 15:34:00 -0500

    Adding another example:

---
+++ Week_4/Composition.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+/*
+* In this file, we're looking at an example of composition!
+* Remember, composition is a "has-a" relationship. A car 
+* has an engine!
+*/
+
+class Engine {
+public:
+	void start(){
+		std::cout << "Engine started!" << std::endl;
+	}
+};
+
+class Car {
+	// Car HAS an Engine
+	Engine engine;
+	std::string brand;
+public:
+	Car(std::string b){
+		brand = b;
+	}
+
+	void start(){
+		engine.start();
+		std::cout << "Vroom vroom!" << std::endl;
+    }
+};
+
+int main(){
+	Car myCar("Toyota");
+	// Note the output
+	myCar.start();
+}
